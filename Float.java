7a90aace6bb8476afc4931d58612b0cd54a8164b
Rithikaa Redde <141546241+rithikaaredde@users.noreply.github.com>
2025-03-06 16:01:57 +0530

    updated Float.java

--- Float.java
+++ Float.java
@@ -1,2 +1,10 @@
-public class Float {
+// Float.java
+
+public class Float implements SwimBehavior
+{
+    @Override
+    public void swim()
+    {
+        System.out.println("I float on water.");
+    }
 }
