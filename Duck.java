c421340b934cf9718832c8ca333ffdf7eb2e803e
Rithikaa Redde <141546241+rithikaaredde@users.noreply.github.com>
2025-03-06 15:55:38 +0530

    added code

--- Duck.java
+++ Duck.java
@@ -1,2 +1,21 @@
-public class Duck {
+// Duck.java
+
+public abstract class Duck
+{
+    protected FlyBehaviour flyBehaviour;
+    protected SwimBehavior swimBehavior;
+
+    public Duck() {}
+
+    public void performFly()
+    {
+        flyBehaviour.fly();
+    }
+
+    public void performSwim()
+    {
+        swimBehavior.swim();
+    }
+
+    public abstract void display();
 }
