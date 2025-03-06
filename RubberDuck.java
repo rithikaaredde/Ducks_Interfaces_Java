c8141aed2611246845257def287e9a321a504611
Rithikaa Redde <141546241+rithikaaredde@users.noreply.github.com>
2025-03-06 15:57:22 +0530

    added code

--- RubberDuck.java
+++ RubberDuck.java
@@ -1,2 +1,15 @@
-public class RubberDuck {
+// RubberDuck.java
+
+public class RubberDuck extends Duck
+{
+    public RubberDuck() {
+        flyBehaviour = new NotFly();
+        swimBehavior = new Float();
+    }
+
+    @Override
+    public void display()
+    {
+        System.out.println("I am a Rubber Duck.");
+    }
 }
