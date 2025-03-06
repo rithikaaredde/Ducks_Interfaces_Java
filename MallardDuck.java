97132f857667a4b69bfb76a38660234b0b2b6cb6
Rithikaa Redde <141546241+rithikaaredde@users.noreply.github.com>
2025-03-06 15:56:16 +0530

    added code

--- MallardDuck.java
+++ MallardDuck.java
@@ -1,2 +1,16 @@
-public class MallardDuck {
+// MallardDuck.java
+
+public class MallardDuck extends Duck
+{
+    public MallardDuck()
+    {
+        flyBehaviour = new Fly();
+        swimBehavior = new Swim();
+    }
+
+    @Override
+    public void display()
+    {
+        System.out.println("I am a Mallard Duck.");
+    }
 }
